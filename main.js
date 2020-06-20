32f5dd77d7fbea68d2669e95aa0c764b929a9496
janko12 <henrich.sury97@gmail.com>
2020-06-20 18:12:13 +0200

    ammended operate function, so the Math with 1 digit numbers works

--- main.js
+++ main.js
@@ -10,27 +10,103 @@ const btnContainer = document.querySelector("#btn-container");
 numbers.forEach(btn => btn.addEventListener("click",e => userInput.value += `${e.target.textContent}`));
 operators.forEach(btn => btn.addEventListener("click",e => userInput.value += ` ${e.target.textContent} `));
 clrBtn.addEventListener("click",() => userInput.value="");
+eqlBtn.addEventListener("click", getResult);
+// window.addEventListener("keypress",getResult);
 
 
 
 
-function operate(operator, num1, num2){
-    switch(operator){
-        case "+":
-            return add(mum1,num2);
-        case "-":
-            return subtract(num1,num2);
-        case "*":
-            return multiply(num1,num2);
-        case "/":
-            return divide(num1,num2); 
+
+//function section
+
+function getResult(){
+    
+    let inpt = userInput.value.replace(/ /g,"") + "";
+   
+    // bug reports
+    if(inpt === "")
+    {
+        alert("input has to be mathematic expression");
+        return;
     }
+    if(inpt.match(/^[*/]/))
+    {
+        alert("input must not start with * or /");
+        return;
+    }
+    if(inpt.match(/[^0-9 +-/*]/g))
+    {
+        alert("input must not contain other than calculator symbols");
+        return;
+    }
+    if(isNaN(inpt[inpt.length-1]))
+    {
+        alert("input must end with number");
+        return;
+    }
+    if(inpt.match(/[-+*/]{2,}/))
+    {
+        alert("input must not contain more than 1 sign after one another");
+        return;
+    }
+    
+    // ignores all + and - and evaluates all * and / expressions
+    let store = inpt.split(/[-+]/);
+    store = store.map(operate)
+
+    
+    // putting + and - to the places where they belong (replacing spaces) and evaluates the rest of the expression
+    store = store.join(" ").split("");
+    let plusMinusSigns = inpt.match(/[-+]/g);
+
+    for (let i = index = 0; i < store.length; i++)
+        if(store[i] === " ")
+            store[i] = plusMinusSigns[index++];
+    
+    
+    let result = [store.join("")].map(operate).join("");
+    userInput.value = result;
+   
 }
 
-// operator functions
+function operate(x)
+{
+    let left = x[0];
+    let operand = "";
+    let right = "";
+    for (let i = 1; i < x.length; i++) {
+        
+        if(i%2 === 1)
+        operand = x[i];
+        else
+        {
+            right = x[i];
+
+            switch(operand)
+            {
+                case "+":
+                    left = add(left,right);
+                    break;
+                case "-":
+                    left = subtract(left,right);
+                    break;
+                case "*":
+                    left = multiply(left,right);
+                    break;
+                case "/":
+                    left = divide(left,right);
+                    break;
+            }
+
+        }
+    }
+    return left;
+}
+
+// operator functions (add is *1 bcs of string)
 
 function add(){
-    return [...arguments].reduce((a,b) => a + b);
+    return [...arguments].reduce((a,b) => a*1 + b*1);
 }
 function subtract(){
     return [...arguments].reduce((a,b) => a - b);
@@ -41,3 +117,4 @@ function multiply(){
 function divide(){
     return [...arguments].reduce((a,b) => a / b);
 }
+
