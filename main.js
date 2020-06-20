bbd24707de314f278cf03a29384df7a0623239a0
janko12 <henrich.sury97@gmail.com>
2020-06-20 19:24:54 +0200

    ammended operate function to the final funcionality with larger numbers

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ clrBtn.addEventListener("click",() => userInput.value="");
 eqlBtn.addEventListener("click", getResult);
 // window.addEventListener("keypress",getResult);
 
-
+console.log("-5.2"-"2");
 
 
 
@@ -24,45 +24,37 @@ function getResult(){
     let inpt = userInput.value.replace(/ /g,"") + "";
    
     // bug reports
-    if(inpt === "")
-    {
-        alert("input has to be mathematic expression");
-        return;
-    }
-    if(inpt.match(/^[*/]/))
-    {
-        alert("input must not start with * or /");
-        return;
-    }
-    if(inpt.match(/[^0-9 +-/*]/g))
-    {
-        alert("input must not contain other than calculator symbols");
-        return;
-    }
-    if(isNaN(inpt[inpt.length-1]))
-    {
-        alert("input must end with number");
-        return;
-    }
-    if(inpt.match(/[-+*/]{2,}/))
-    {
-        alert("input must not contain more than 1 sign after one another");
-        return;
-    }
+    if(inpt === "")                {alert("input has to be mathematic expression");return;}
+    if(inpt.match(/^[*/]/))        {alert("input must not start with * or /");return;}
+    if(inpt.match(/[^0-9 +-/*]/g)) {alert("input must not contain other than calculator symbols");return;}
+    if(isNaN(inpt[inpt.length-1])) {alert("input must end with number");return;}
+    if(inpt.match(/[-+*/]{2,}/))   {alert("input must not contain more than 1 sign after one another");return;}
     
     // ignores all + and - and evaluates all * and / expressions
     let store = inpt.split(/[-+]/);
-    store = store.map(operate)
+    if(inpt[0] === "-" || inpt[0] === "+")
+    {
+        store.shift();
+        store[0] = inpt[0] + store[0];
+        inpt = inpt.split("");
+        inpt.shift();
+        inpt = inpt.join("");
+    }
+    console.log(inpt);
+    store = store.map(operate);
+    console.log(store);
 
     
     // putting + and - to the places where they belong (replacing spaces) and evaluates the rest of the expression
     store = store.join(" ").split("");
     let plusMinusSigns = inpt.match(/[-+]/g);
+    console.log(store);
 
     for (let i = index = 0; i < store.length; i++)
         if(store[i] === " ")
             store[i] = plusMinusSigns[index++];
     
+    console.log(store);
     
     let result = [store.join("")].map(operate).join("");
     userInput.value = result;
@@ -71,16 +63,22 @@ function getResult(){
 
 function operate(x)
 {
-    let left = x[0];
+    
+
+    let left = x.match(/[-+]?[0-9.]+/) + "";
     let operand = "";
     let right = "";
-    for (let i = 1; i < x.length; i++) {
+    let counter = 0;
+    for (let i = left.length; i < x.length; counter++) {
         
-        if(i%2 === 1)
-        operand = x[i];
+        if(counter%2 === 0)
+        {
+            operand = x[i];
+            i++;
+        }
         else
         {
-            right = x[i];
+            right = x.slice(i).match(/[0-9.]+/);
 
             switch(operand)
             {
@@ -97,7 +95,7 @@ function operate(x)
                     left = divide(left,right);
                     break;
             }
-
+            i+=right.length;
         }
     }
     return left;
